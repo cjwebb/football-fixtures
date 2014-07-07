908dfd4664c73e7f56897795808c777b4ef01fe8
Colin Webb <colin.web@net-a-porter.com>
2014-07-07 08:32:42 +0100

    fixing test

--- test/football_fixtures/test/handler.clj
+++ test/football_fixtures/test/handler.clj
@@ -1,11 +1,13 @@
 (ns football-fixtures.test.handler
   (:use clojure.test
         ring.mock.request  
-        football-fixtures.handler))
+        football-fixtures.handler
+        football-fixtures.fixtures))
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (request :get "/fixtures"))]
+  (testing "main route returns fixtures-atom"
+    (let [fixture-list (reset! fixtures-atom {:hello "world"})
+          response (app (request :get "/fixtures"))]
       (is (= (:status response) 200))
       (is (= (:body response) "{\"hello\":\"world\"}"))))
   
